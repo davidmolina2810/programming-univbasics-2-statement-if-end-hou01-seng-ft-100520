8ca40937ffb881dc992ce56b7410aae54bed1bda
David Molina <davidmolina2810@gmail.com>
2020-08-08 05:09:54 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -3,8 +3,10 @@
 now = Time.now
 now = now.to_i
 
-if (now % 2 == 0)
-  puts "Even!"
-else
-  puts "Odd!"
+def not_using_even?(now)
+  if (now % 2 == 0)
+    puts "Even!"
+  else
+    puts "Odd!"
+  end
 end
